ad4fa148e207fc6549911639989b2c0525ea4053
12605353982@edu.udesc.br <12605353982@edu.udesc.br>
2021-08-27 10:36:09 -0300

    add contato

--- helper.js
+++
@@ -1,6 +0,0 @@
-
-function verificarLogin(){
-    if(localStorage._id==null){
-      window.location.href = '/login.html';
-      alert("Você precisa se logar ou se registrar para acessar essa página");
-    }
